5cd4f546fe0c58fb6cf8a3e211c5589e5d03e8d6
FarisAnugrah <farisakun65@gmail.com>
2026-09-11 10:06:42 +0700

    chore: add sepolia network support for easier faucet access

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const privateKey = process.env.PRIVATE_KEY;
+const isValidKey = privateKey && privateKey.length === 66 && privateKey.startsWith("0x") || privateKey && privateKey.length === 64;
+const accountArr = isValidKey ? [(privateKey.startsWith("0x") ? privateKey : `0x${privateKey}`)] : [];
+
 module.exports = {
   solidity: "0.8.20",
   networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: accountArr,
+    },
     polygonAmoy: {
       url: process.env.ALCHEMY_AMOY_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      accounts: accountArr,
     }
   }
 };
